f1975874296892d434f91b8f5bdcebeba0aa827e
Ishan Veer <ishanpveer@gmail.com>
2022-08-25 18:44:31 +0530

    made the cart modal working

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 
 import classes from "./Modal.module.css";
 
-const Backdrop = () => {
-  return <div className={classes.backdrop}></div>;
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
 };
 const ModalOverlay = (props) => {
   return (
@@ -17,7 +17,7 @@ const Modal = (props) => {
   const portalElement = document.getElementById("overlays");
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClick={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
